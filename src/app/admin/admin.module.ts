7d24dd2ecf22f8bcfa686cfe2412baee16ae7da1
Maximilian Barabas <barabasmaximilian@gmail.com>
2023-03-14 20:34:15 +0100

    download_adminlte_plugins

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { ProductsComponent } from './products/products.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsidebarComponent } from './asidebar/asidebar.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    ProductsComponent
+    ProductsComponent,
+    SidebarComponent,
+    TopbarComponent,
+    FooterComponent,
+    AsidebarComponent
   ],
   imports: [
     CommonModule,
